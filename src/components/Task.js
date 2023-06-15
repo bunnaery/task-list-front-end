d81cb9fa22edd2153bef7cead85a12ac82b59fc2
Sage Choi <coolcuds@gmail.com>
2023-06-15 15:27:34 -0700

    Move state from Task to App

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+const Task = ({ id, title, isComplete, onUpdateTask }) => {
+  const onTaskClick = () => {
+    const updatedTask = {
+      id: id,
+      title: title,
+      isComplete: !isComplete
+    };
+
+    onUpdateTask(updatedTask);
+  };
+
+  const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        onClick={onTaskClick}
       >
         {title}
       </button>
@@ -24,6 +33,7 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  onUpdateTask: PropTypes.func.isRequired
 };
 
 export default Task;
