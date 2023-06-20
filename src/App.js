d59364ae61fc0cd9d6e9127b64a4a9bbd6ad8b36
Sage Choi <coolcuds@gmail.com>
2023-06-20 14:19:46 -0700

    Added axios routes for API calls

--- src/App.js
+++ src/App.js
@@ -1,37 +1,90 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TaskList from './components/TaskList.js';
+import axios from 'axios';
 import './App.css';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+// const URL = 'https://coolcud-task-list-api.onrender.com';
+// const URL = 'https://task-list-api-c17.onrender.com/';
 
+// Data
+// const TASKS = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
+
+const mapTasks = (responseData) => {
+  const taskData = responseData.data.map((task) => {
+    return {
+      id: task.id,
+      title: task.title,
+      isComplete: task.is_complete
+    };
+  });
+
+  return taskData;
+};
+
+// App
 const App = () => {
-  const [taskData, setTaskData] = useState(TASKS);
-  const updateTask = updatedTask => {
-    const tasks = taskData.map(task => {
-      if (task.id === updatedTask.id) {
-        return updatedTask;
-      } else {
-        return task;
-      }
-    });
+  const [taskData, setTaskData] = useState([]);
 
-    setTaskData(tasks);
+  const getTasks = () => {
+    axios
+      .get(`${URL}/tasks`)
+      .then((response) => {
+        const newTasks = mapTasks(response);
+        setTaskData(newTasks);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  // const updateTask = updatedTask => {
+  //   const tasks = taskData.map(task => {
+  //     if (task.id === updatedTask.id) {
+  //       return updatedTask;
+  //     } else {
+  //       return task;
+  //     }
+  //   });
+
+  //   setTaskData(tasks);
+  // };
+
+  const toggleCompleteTask = task => {
+    let newURL = '';
+    if (task.isComplete) {
+      newURL = `${URL}/tasks/${task.id}/mark_complete`;
+    } else {
+      newURL = `${URL}/tasks/${task.id}/mark_incomplete`;
+    }
+
+    axios
+      .patch(newURL)
+      .then(() => {
+        getTasks();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   const deleteTask = id => {
-    setTaskData(oldData => {
-      return oldData.filter(task => task.id != id);
+    setTaskData(oldTasks => {
+      return oldTasks.filter(task => task.id != id);
     });
   };
 
@@ -44,7 +97,7 @@ const App = () => {
         <div>
           {<TaskList
             tasks={taskData}
-            onUpdateTask={updateTask}
+            onUpdateTask={toggleCompleteTask}
             onDeleteTask={deleteTask}
           />}
         </div>
